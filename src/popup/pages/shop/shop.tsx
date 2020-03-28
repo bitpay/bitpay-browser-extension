db0a195aff8e999df70eca3fd3d80b9f64f9bd80
Ash Bhimasani <abhimasani@bitpay.com>
2020-03-28 15:27:05 -0400

    feat(merchant cell): add merchant cell component for global usee

--- src/popup/pages/shop/shop.tsx
+++ src/popup/pages/shop/shop.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CardConfig } from '../../../services/gift-card.types';
 
+import MerchantCell from '../../components/merchant-cell/merchant-cell';
+
 const Shop: React.FC<{ merchants: CardConfig[] }> = ({ merchants }) => (
   <div>
     <div>Shop ({merchants?.length})</div>
     {merchants.map(merchant => (
       <Link to={`brand/${merchant.name}`} key={merchant.name}>
-        <div>{merchant.displayName}</div>
+        <MerchantCell
+          key={merchant.name}
+          avatar={merchant.icon}
+          displayName={merchant.displayName}
+          currency={merchant.currency}
+          minAmount={merchant.minAmount}
+          maxAmount={merchant.maxAmount}
+          amounts={merchant.supportedAmounts}
+        />
       </Link>
     ))}
   </div>
