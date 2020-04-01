a81a7cefe505428902fba79aad0bff06c8b53264
Ash Bhimasani <abhimasani@bitpay.com>
2020-04-01 16:54:17 -0400

    feat(merchant cell): add discount logic to merchant cell, minor refactor of css
    Signed-off-by: Ash Bhimasani <abhimasani@bitpay.com>

--- src/popup/components/merchant-cell/merchant-cell.tsx
+++ src/popup/components/merchant-cell/merchant-cell.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './merchant-cell.scss';
-import { Merchant } from '../../../services/merchant';
+import { Merchant, formatDiscount } from '../../../services/merchant';
 import CardDenoms from '../card-denoms/card-denoms';
 
 const MerchantCell: React.FC<{ merchant: Merchant }> = ({ merchant }) => {
   const cardConfig = merchant.giftCards[0];
   return (
-    <div className="MerchantCell">
-      <img className="MerchantCell__Avatar" alt="amazon" src={merchant.icon} />
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <div className="MerchantCell__Title">{merchant.displayName}</div>
-        <div className="MerchantCell__Caption">
-          {merchant.hasDirectIntegration ? <>{merchant.caption}</> : <CardDenoms cardConfig={cardConfig} />}
-        </div>
+    <div className="merchant-cell">
+      <img className="merchant-cell__avatar" alt="amazon" src={merchant.icon} />
+      <div className="merchant-cell__block">
+        <div className="merchant-cell__title">{merchant.displayName}</div>
+        {merchant.discount ? (
+          <div className="merchant-cell__caption" style={{ color: merchant.theme, fontWeight: 500 }}>
+            {formatDiscount(merchant.discount, cardConfig ? cardConfig.currency : merchant.discount.currency)} Off Every
+            Purchase
+          </div>
+        ) : (
+          <div className="merchant-cell__caption">
+            {merchant.hasDirectIntegration ? <>{merchant.caption}</> : <CardDenoms cardConfig={cardConfig} />}
+          </div>
+        )}
       </div>
     </div>
   );
