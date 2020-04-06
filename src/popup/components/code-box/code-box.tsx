185a443cc2f644b57f3c7f35744aaed698cdb817
Ash Bhimasani <abhimasani@bitpay.com>
2020-04-05 21:08:55 -0400

    feat(card details): prototype card details animation with manual style control
    Signed-off-by: Ash Bhimasani <abhimasani@bitpay.com>

--- src/popup/components/code-box/code-box.tsx
+++ src/popup/components/code-box/code-box.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './code-box.scss';
 
 import copyUtil from '../../../services/copy-util';
 
-const CodeBox: React.FC<{ code: string; label: string }> = ({ code, label }) => (
-  <div className="code-box--wrapper">
-    <button className="code-box" onClick={(): void => copyUtil(code)} type="button">
-      <div className="code-box__value">{code}</div>
-      <div className="code-box__label">{label}</div>
-      <div className="code-box__label code-box__label--action">Copy to Clipboard</div>
-    </button>
-  </div>
-);
+const CodeBox: React.FC<{ code: string; label: string }> = ({ code, label }) => {
+  const box = useRef<HTMLButtonElement>(null);
+  const value = useRef<HTMLDivElement>(null);
+  const [copied, setCopied] = useState(false);
+  const boxClicked = (val: string): void => {
+    copyUtil(val);
+    if (!copied && box.current && value.current) {
+      setCopied(true);
+      box.current.style.transform = 'scale(1.03)';
+      value.current.style.color = '#4f6ef7';
+      setTimeout((): void => {
+        box.current.style.transform = 'initial';
+      }, 300);
+      setTimeout((): void => {
+        value.current.style.color = 'initial';
+        setCopied(false);
+      }, 1500);
+    }
+  };
+  return (
+    <div className="code-box--wrapper">
+      <button className="code-box" onClick={(): void => boxClicked(code)} ref={box} type="button">
+        <div className="code-box__value" ref={value}>
+          {code}
+        </div>
+        <div className="code-box__label">{label}</div>
+        <div className="code-box__label code-box__label--action">Copy to Clipboard</div>
+      </button>
+    </div>
+  );
+};
 
 export default CodeBox;
