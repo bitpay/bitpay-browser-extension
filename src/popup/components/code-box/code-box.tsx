0a371b0c62ac95a12b715b12f466b3f10618253d
Ash Bhimasani <abhimasani@bitpay.com>
2020-04-07 14:42:07 -0400

    fix(copy animation): prototype improved copy animation, wip reduce rerenders on state change
    Signed-off-by: Ash Bhimasani <abhimasani@bitpay.com>

--- src/popup/components/code-box/code-box.tsx
+++ src/popup/components/code-box/code-box.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './code-box.scss';
 
 import Anime, { anime } from 'react-anime';
@@ -6,21 +6,46 @@ import copyUtil from '../../../services/copy-util';
 
 const CodeBox: React.FC<{ code: string; label: string }> = ({ code, label }) => {
   const [hovering, setHovering] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const startCopying = useCallback(() => {
+    if (copied) return;
+    setCopied(true);
+    setTimeout((): void => {
+      setCopied(false);
+      setHovering(false);
+    }, 1500);
+  }, [copied]);
+  const handleHovering = useCallback(
+    (newVal: boolean) => {
+      if (copied) return;
+      setHovering(newVal);
+    },
+    [copied]
+  );
   return (
     <div className="code-box--wrapper">
       <button
         className="code-box"
-        onClick={(): void => copyUtil(code)}
-        onMouseEnter={(): void => setHovering(true)}
-        onMouseLeave={(): void => setHovering(false)}
+        onClick={(): void => {
+          copyUtil(code);
+          startCopying();
+        }}
+        onMouseEnter={(): void => handleHovering(true)}
+        onMouseLeave={(): void => handleHovering(false)}
         type="button"
       >
         <div className="code-box__value">{code}</div>
         <Anime delay={anime.stagger(50)} translateY={[8, 0]} opacity={[0, 1]}>
-          {hovering ? (
-            <div className="code-box__label code-box__label--action">Copy to Clipboard</div>
+          {copied ? (
+            <div className="code-box__label code-box__label--action">Copied to Clipboard!</div>
           ) : (
-            <div className="code-box__label">{label}</div>
+            <>
+              {hovering ? (
+                <div className="code-box__label code-box__label--action">Copy to Clipboard</div>
+              ) : (
+                <div className="code-box__label">{label}</div>
+              )}
+            </>
           )}
         </Anime>
       </button>
