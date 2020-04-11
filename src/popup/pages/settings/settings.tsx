78a4b7156a0d8ecfe8e951c3d8cae2427f46677a
Ash Bhimasani <abhimasani@bitpay.com>
2020-04-11 13:27:14 -0400

    refactor(settings): removed unused rows in settings
    Signed-off-by: Ash Bhimasani <abhimasani@bitpay.com>

--- src/popup/pages/settings/settings.tsx
+++ src/popup/pages/settings/settings.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './settings.scss';
 import { Link } from 'react-router-dom';
 import { resizeFrame } from '../../../services/frame';
+import { launchNewTab } from '../../../services/browser';
 
 const Settings: React.FC<{ email: string }> = ({ email }) => {
   resizeFrame(450);
@@ -9,7 +10,6 @@ const Settings: React.FC<{ email: string }> = ({ email }) => {
     <div className="settings">
       <div className="settings-group">
         <div className="settings-group__label">Wallet</div>
-        <div className="settings-group__item">Hide Empty Balances</div>
         <Link type="button" className="settings-group__item" to="/settings/archive">
           Archived Gift Cards
         </Link>
@@ -23,17 +23,17 @@ const Settings: React.FC<{ email: string }> = ({ email }) => {
       </div>
       <div className="settings-group">
         <div className="settings-group__label">Other</div>
-        <button type="button" className="settings-group__item">
-          <div className="settings-group__item__label">Country</div>
-          <div className="settings-group__item__value">USA</div>
-        </button>
         <Link type="button" className="settings-group__item" to="/settings/legal">
           Legal
         </Link>
-        <div className="settings-group__item">
+        <button
+          type="button"
+          className="settings-group__item"
+          onClick={(): void => launchNewTab('https://github.com/msalcala11/extension')}
+        >
           <div className="settings-group__item__label">Version</div>
           <div className="settings-group__item__value">1.0.0</div>
-        </div>
+        </button>
       </div>
     </div>
   );
