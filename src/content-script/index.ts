7eaff53fad584074c33d6fac7457d0df79fe6c14
L S <lshivananda@bitpay.com>
2020-04-29 14:20:23 -0400

    feat(content-script): allow drag and drop extension window

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -5,13 +5,14 @@ import { Merchant } from '../services/merchant';
 import { CheckoutPageCssSelectors } from '../services/gift-card.types';
 
 let iframe: HTMLIFrameElement | undefined;
+const DragElementId = 'BitPayExtensionDrag';
 
 const getOrderTotal = (selector: string): number | undefined => {
   const element = document.querySelector(selector);
   return element ? parseFloat(element.innerHTML.trim().replace(/[^0-9.]/g, '')) : undefined;
 };
 
-function getIframeStyles(): { outerFrameStyles: string; innerFrameStyles: string } {
+function getIframeStyles(): { outerFrameStyles: string; innerFrameStyles: string; dragElementStyles: string } {
   const innerFrameStyles = `
     width: 100%;
     height: 100%; 
@@ -32,10 +33,24 @@ function getIframeStyles(): { outerFrameStyles: string; innerFrameStyles: string
     right: 10px;
     box-shadow: 0 0 14px 4px rgba(0,0,0,0.1); 
     border-radius: 8px;
+<<<<<<< Updated upstream
     z-index: 2147483647!important;
+=======
+    z-index: ${FrameDimensions.zIndex};
+>>>>>>> Stashed changes
     transition: height 250ms ease 0s;
   `;
-  return { outerFrameStyles, innerFrameStyles };
+  const dragElementStyles = `
+    width: 160px; 
+    height: ${FrameDimensions.collapsedHeight}px; 
+    position: fixed;
+    top: 10px;
+    right: 75px;
+    display: block;
+    cursor: move;
+    z-index: ${FrameDimensions.zIndex + 10};
+   `;
+  return { outerFrameStyles, innerFrameStyles, dragElementStyles };
 }
 
 function createIframe({ merchant }: { merchant?: Merchant }): HTMLIFrameElement {
@@ -67,8 +82,119 @@ function addIframe(frame: HTMLIFrameElement): void {
   document.body.appendChild(frame);
 }
 
+// Extension Drag
+function getDragElement(): HTMLElement | null {
+  return document.getElementById(DragElementId);
+}
+
+function removeDragElement(): void {
+  const el = getDragElement();
+  if (el) {
+    document.body.removeChild(el);
+  }
+}
+
+function dragElementFunc(dragEle: HTMLElement): void {
+  let pos1 = 0;
+  let pos2 = 0;
+  let pos3 = 0;
+  let pos4 = 0;
+  const windowInnerHeight = window.innerHeight;
+  const windowInnerWidth = window.innerWidth;
+  const paddingX = 75;
+  const paddingY = 10;
+  let rect: ClientRect;
+  const viewport = {
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function elementDrag(e: any): void {
+    // eslint-disable-next-line no-param-reassign
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    const newLeft = dragEle.offsetLeft - pos1;
+    const newTop = dragEle.offsetTop - pos2;
+
+    const leftBound = newLeft < viewport.left;
+    const topBound = newTop < viewport.top;
+    const rightBound = newLeft + rect.width > viewport.right;
+    const bottomBound = newTop + rect.height > viewport.bottom;
+    if (leftBound || topBound || rightBound || bottomBound) {
+      if (rightBound && !topBound && !bottomBound) {
+        browser.runtime.sendMessage({
+          name: 'RESET_FRAME_POSITION',
+          top: newTop,
+          left: windowInnerWidth - FrameDimensions.width - 10
+        });
+        dragEle.style.top = `${newTop}px`;
+        dragEle.style.left = `${window.innerWidth - 235}px`;
+      }
+    } else {
+      // set the element's new position:
+      browser.runtime.sendMessage({ name: 'RESET_FRAME_POSITION', top: newTop, left: newLeft - paddingX });
+      dragEle.style.top = `${newTop}px`;
+      dragEle.style.left = `${newLeft}px`;
+    }
+  }
+
+  function closeDragElement(): void {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-function-return-type
+  function dragMouseDown(e: any) {
+    // eslint-disable-next-line no-param-reassign
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    rect = dragEle.getBoundingClientRect();
+    viewport.bottom = windowInnerHeight - paddingY;
+    viewport.left = paddingX;
+    viewport.right = windowInnerWidth - paddingX;
+    viewport.top = paddingY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+  dragEle.onmousedown = dragMouseDown;
+}
+
+function addDragElement(): void {
+  const dragElement = document.createElement('div');
+  const { dragElementStyles } = getIframeStyles();
+  dragElement.style.cssText = dragElementStyles;
+  dragElement.classList.add('drag-element');
+  dragElement.setAttribute('id', DragElementId);
+  document.body.appendChild(dragElement);
+
+  const dragEle = getDragElement();
+  if (dragEle) {
+    dragElementFunc(dragEle);
+  }
+}
+
 function resizeIframe(frame: HTMLIFrameElement, height: number = FrameDimensions.height): void {
   frame.style.height = `${height}px`;
+}
+
+function resetIframePosition(frame: HTMLIFrameElement, top: number, left: number): void {
+  frame.style.top = `${top}px`;
+  frame.style.left = `${left}px`;
 }
 
 function injectValueIntoInputsWithSelectors(selectors: string[], value: string): void {
@@ -82,7 +208,14 @@ browser.runtime.onMessage.addListener(async message => {
   const messageName = message && message.name;
   switch (messageName) {
     case 'EXTENSION_ICON_CLICKED':
-      return iframe ? removeIframe(iframe) : addIframe(createIframe({ merchant: message.merchant }));
+      if (iframe) {
+        removeDragElement();
+        return removeIframe(iframe);
+      }
+      addIframe(createIframe({ merchant: message.merchant }));
+      addDragElement();
+      return;
+
     case 'INJECT_CLAIM_INFO':
       // eslint-disable-next-line no-case-declarations
       const { cssSelectors, claimInfo } = message as {
@@ -93,9 +226,12 @@ browser.runtime.onMessage.addListener(async message => {
       if (claimInfo.pin) injectValueIntoInputsWithSelectors(cssSelectors.pinInput, claimInfo.pin);
       return;
     case 'POPUP_CLOSED':
+      removeDragElement();
       return iframe && removeIframe(iframe);
     case 'POPUP_RESIZED':
       return iframe && resizeIframe(iframe, message.height);
+    case 'RESET_FRAME_POSITION':
+      return iframe && resetIframePosition(iframe, message.top, message.left);
     default:
       console.log('Unsupported Event:', message);
   }
